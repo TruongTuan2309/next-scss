cbfc8a841e6f5641e0045a10a1583a66eb33ed93
truongtuan2309 <tranhtuan2309@gmail.com>
2024-11-10 15:45:35 +0700

    init project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: ['./src/styles']
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            namedExport: 'ReactComponent'
+          }
+        }
+      ]
+    })
+    return config
+  }
+}
 
-export default nextConfig;
+export default nextConfig
